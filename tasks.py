0bac875314a57721ec731c03870a4cc8627ac4d5
Nejc Zupan <nejc.zupan@gmail.com>
2014-02-18 10:40:09 +0100

    different Redis provider, more logging, waveriderz task

--- tasks.py
+++ tasks.py
@@ -23,7 +23,7 @@ VARS = [
     'MAILGUN_SMTP_PASSWORD',
     'MAILGUN_SMTP_PORT',
     'MAILGUN_SMTP_SERVER',
-    'REDISTOGO_URL',
+    'REDISCLOUD_URL',
 ]
 
 
@@ -37,7 +37,7 @@ app = Celery('tasks')
 
 # Use Redis as our broker and define json as the default serializer
 app.conf.update(
-    BROKER_URL=os.environ['REDISTOGO_URL'],
+    BROKER_URL=os.environ['REDISCLOUD_URL'],
     CELERY_TASK_SERIALIZER='json',
     CELERY_ACCEPT_CONTENT=['json', 'msgpack', 'yaml'],
     CELERYD_CONCURRENCY=1,
@@ -67,15 +67,19 @@ def totisurf():
 
     curr_modified = soup.find('font', color='red').text
 
-    db = redis.StrictRedis.from_url(os.environ['REDISTOGO_URL'])
+    db = redis.StrictRedis.from_url(os.environ['REDISCLOUD_URL'])
     if not db.get('totisurf_modified'):
-        logging.info('Forecast not found in DB, storing modified date to DB.')
+        logging.info('totisurf_modified not found in DB, storing "{}".'.format(
+            curr_modified))
         db.set('totisurf_modified', curr_modified)
         return
 
     old_modified = db.get('totisurf_modified')
     if old_modified != curr_modified:
-        logging.info('Forecast was modified, sending email.')
+        logging.info(
+            'totisurf_modified modified, sending email, storing "{}".'.format(
+                curr_modified))
+
         db.set('totisurf_modified', curr_modified)
 
         forecast = soup.find(class_='vreme')
@@ -88,6 +92,49 @@ def totisurf():
             To='nejc.zupan@gmail.com',
             Subject="Totisurf forecast changed",
             Html=str(forecast),
+        )
+
+        sender = Mailer(
+            host=os.environ['MAILGUN_SMTP_SERVER'],
+            port=os.environ['MAILGUN_SMTP_PORT'],
+            usr=os.environ['MAILGUN_SMTP_LOGIN'],
+            pwd=os.environ['MAILGUN_SMTP_PASSWORD'],
+            use_tls=True,
+        )
+        sender.send(message)
+    else:
+        logging.info('No changes.')
+
+
+@periodic_task(run_every=timedelta(seconds=15))
+def waveriderz():
+    response = requests.get('http://waveriderz.wordpress.com/')
+    assert response.status_code == 200
+
+    from bs4 import BeautifulSoup
+    soup = BeautifulSoup(response.text)
+
+    curr_forecast = soup.find('div', class_='textwidget').text
+
+    db = redis.StrictRedis.from_url(os.environ['REDISTOGO_URL'])
+    if not db.get('waveriderz_forecast'):
+        logging.info('waveriderz_forecast not found in DB, storing "{}".'.format(  # noqa
+            curr_forecast))
+        db.set('waveriderz_forecast', curr_forecast)
+        return
+
+    old_forecast = db.get('waveriderz_forecast')
+    if old_forecast != curr_forecast:
+        logging.info(
+            'waveriderz_forecast modified, sending email, storing "{}".'.format(  # noqa
+                curr_forecast))
+        db.set('waveriderz_forecast', curr_forecast)
+
+        message = Message(
+            From=os.environ['MAILGUN_SMTP_LOGIN'],
+            To='nejc.zupan@gmail.com',
+            Subject="Waveriderz forecast changed",
+            Html=u'Borut pravi: <br />' + curr_forecast,
         )
 
         sender = Mailer(
